edbb6d7d71b912c35674cf60f12293c30d7a0c7e
sntgrazi <sntgrazi22@gmail.com>
2023-10-10 22:03:24 -0300

    ajustando html e modulo

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -2,13 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ConteudoComponent } from './components/conteudo/conteudo.component';
+import { BoxEsquerdoComponent } from './components/conteudo/box-esquerdo/box-esquerdo.component';
+import { BoxDireitoComponent } from './components/conteudo/box-direito/box-direito.component';
+import { ResumoComponent } from './components/conteudo/resumo/resumo.component';
+import { DiscussoesComponent } from './components/conteudo/discussoes/discussoes.component';
+import { CardComponent } from './components/conteudo/discussoes/card/card.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    NavbarComponent,
+    ConteudoComponent,
+    BoxEsquerdoComponent,
+    BoxDireitoComponent,
+    ResumoComponent,
+    DiscussoesComponent,
+    CardComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
