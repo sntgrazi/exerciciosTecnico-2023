a5105f62532198a39cdd1c3ca8aa78f7c92bede5
sntgrazi <sntgrazi22@gmail.com>
2023-10-11 00:07:15 -0300

    ajustes

--- ts/src/app/components/conteudo/discussoes/discussoes.component.ts
+++ ts/src/app/components/conteudo/discussoes/discussoes.component.ts
@@ -12,30 +12,30 @@ export class DiscussoesComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  mostrarFormulario = false; // Variável para controlar a exibição do formulário
+  mostrarFormulario = false; 
   enviadoComSucesso: boolean = false;
   mostrarCard: boolean = false;
   cardExpandido: boolean = false;
 
-  // Função para alternar a exibição do formulário
+
   toggleFormulario() {
     this.mostrarFormulario = !this.mostrarFormulario;
   }
 
   enviarFormulario() {
-    // Lógica para enviar o formulário (pode ser uma chamada HTTP)
-    // Após o envio bem-sucedido, defina enviadoComSucesso como true
-    this.enviadoComSucesso = true;
-    this.mostrarCard = true;
-    this.mostrarFormulario = false;
+
+      this.enviadoComSucesso = true;
+      this.mostrarCard = true;
+      this.mostrarFormulario = false;
+  
   }
 
   criarOutroTopico() {
-    // Reinicie o estado do formulário
+   
     this.mostrarFormulario = false;
     this.enviadoComSucesso = false;
     
-    // Mostrar o discussoes-info novamente
+   
     this.toggleFormulario();
   }
 
