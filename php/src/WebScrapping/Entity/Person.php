b70ea42009c852441e45427ed230a5d28dc9ad72
sntgrazi <sntgrazi22@gmail.com>
2023-10-11 00:18:29 -0300

    ajustando identação e comentraio das funcoes

--- php/src/WebScrapping/Entity/Person.php
+++ php/src/WebScrapping/Entity/Person.php
@@ -25,21 +25,40 @@ class Person {
     $this->institution = $institution;
   }
 
+  /**
+   * Obtém o nome da pessoa.
+   *
+   * @return string O nome da pessoa.
+   */
   public function getName() {
       return $this->name;
   }
 
+  /**
+   * Define o nome da pessoa.
+   *
+   * @param string $name O nome a ser atribuído à pessoa.
+   */
   public function setName($name) {
       $this->name = $name;
   }
 
+  /**
+   * Obtém a instituição da pessoa.
+   *
+   * @return string A instituição da pessoa.
+   */
   public function getInstitution() {
       return $this->institution;
   }
 
+  /**
+   * Define a instituição da pessoa.
+   *
+   * @param string $institution A instituição a ser atribuída à pessoa.
+   */
   public function setInstitution($institution) {
       $this->institution = $institution;
   }
-
 
 }
--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -11,103 +11,107 @@ use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 /**
  * Does the scrapping of a webpage.
  */
-class Scrapper {
+class Scrapper
+{
 
-  /**
-   * Loads paper information from the HTML and returns the array with the data.
-   */
-  public function scrap(\DOMDocument $dom): array {
-    $papers = [];
+    /**
+     * Loads paper information from the HTML and returns the array with the data.
+     */
+    public function scrap(\DOMDocument $dom): array
+    {
+        $papers = [];
 
-    $divElements = $dom->getElementsByTagName('div');
-    foreach ($divElements as $divElement) {
-        $classAttribute = $divElement->getAttribute('class');
-        if (strpos($classAttribute, 'col-sm-12 col-md-8 col-lg-8 col-md-pull-4 col-lg-pull-4') !== false) {
-            $paperCards = $divElement->getElementsByTagName('a');
-            foreach ($paperCards as $paperCard) {
-                $paper = $this->extractPaperData($paperCard);
-                if ($paper) {
-                    $papers[] = $paper;
+        $divElements = $dom->getElementsByTagName('div');
+        foreach ($divElements as $divElement) {
+            $classAttribute = $divElement->getAttribute('class');
+            if (strpos($classAttribute, 'col-sm-12 col-md-8 col-lg-8 col-md-pull-4 col-lg-pull-4') !== false) {
+                $paperCards = $divElement->getElementsByTagName('a');
+                foreach ($paperCards as $paperCard) {
+                    $paper = $this->extractPaperData($paperCard);
+                    if ($paper) {
+                        $papers[] = $paper;
+                    }
                 }
             }
         }
+
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+
+        $sheet->setCellValue('A1', 'ID');
+        $sheet->setCellValue('B1', 'Title');
+        $sheet->setCellValue('C1', 'Type');
+
+        $row = 2;
+        $maxAuthors = 0;
+
+        foreach ($papers as $paper) {
+            $authors = $paper->getAuthors();
+            $maxAuthors = max($maxAuthors, count($authors));
+        }
+
+        $column = 'D';
+        for ($i = 1; $i <= $maxAuthors; $i++) {
+            $sheet->setCellValue($column . '1', 'Author ' . $i);
+            $column++;
+            $sheet->setCellValue($column . '1', 'Author ' . $i . ' Institution');
+            $column++;
+        }
+
+        foreach ($papers as $paper) {
+            $this->writePaperDataToSheet($sheet, $paper, $row, $maxAuthors);
+            $row++;
+        }
+
+        $excelFilePath = 'papers.xlsx';
+        $writer = new Xlsx($spreadsheet);
+        $writer->save($excelFilePath);
+
+        return ['message' => 'Planilha gerada com sucesso!!'];
     }
 
-    $spreadsheet = new Spreadsheet();
-    $sheet = $spreadsheet->getActiveSheet();
+    private function extractPaperData($paperCard)
+    {
+        $titleElement = $paperCard->getElementsByTagName('h4')->item(0);
+        $title = $titleElement ? $titleElement->textContent : '';
 
-    $sheet->setCellValue('A1', 'ID');
-    $sheet->setCellValue('B1', 'Title');
-    $sheet->setCellValue('C1', 'Type');
+        $authorElements = $paperCard->getElementsByTagName('div')->item(0)->getElementsByTagName('span');
+        $authors = [];
 
-    $row = 2;
-    $maxAuthors = 0;
+        foreach ($authorElements as $authorElement) {
+            $authorName = trim($authorElement->textContent);
+            $institution = $authorElement->getAttribute('title');
 
-    foreach ($papers as $paper) {
+            $person = new Person($authorName, $institution);
+            $authors[] = $person;
+        }
+
+        $idElement = $paperCard->getElementsByTagName('div')->item(5);
+        $id = $idElement ? $idElement->textContent : '';
+
+        $typeElement = $paperCard->getElementsByTagName('div')->item(2);
+        $type = $typeElement ? $typeElement->textContent : '';
+
+        return new Paper($id, $title, $type, $authors);
+    }
+
+    private function writePaperDataToSheet($sheet, $paper, $row, $maxAuthors)
+    {
+        $sheet->setCellValue('A' . $row, $paper->getId());
+        $sheet->setCellValue('B' . $row, $paper->getTitle());
+        $sheet->setCellValue('C' . $row, $paper->getType());
+
         $authors = $paper->getAuthors();
-        $maxAuthors = max($maxAuthors, count($authors));
+        $column = 'D';
+        for ($i = 0; $i < $maxAuthors; $i++) {
+            if (isset($authors[$i])) {
+                $author = $authors[$i];
+                $sheet->setCellValue($column . $row, $author->getName());
+                $column++;
+                $sheet->setCellValue($column . $row, $author->getInstitution());
+                $column++;
+            }
+        }
     }
-
-    $column = 'D';
-    for ($i = 1; $i <= $maxAuthors; $i++) {
-        $sheet->setCellValue($column . '1', 'Author ' . $i);
-        $column++;
-        $sheet->setCellValue($column . '1', 'Author ' . $i . ' Institution');
-        $column++;
-    }
-
-    foreach ($papers as $paper) {
-        $this->writePaperDataToSheet($sheet, $paper, $row, $maxAuthors);
-        $row++;
-    }
-
-    $excelFilePath = 'papers.xlsx';
-    $writer = new Xlsx($spreadsheet);
-    $writer->save($excelFilePath);
-
-    return ['message' => 'Planilha gerada com sucesso!!'];
-  }
-
-  private function extractPaperData($paperCard) {
-    $titleElement = $paperCard->getElementsByTagName('h4')->item(0);
-    $title = $titleElement ? $titleElement->textContent : '';
-
-    $authorElements = $paperCard->getElementsByTagName('div')->item(0)->getElementsByTagName('span');
-    $authors = [];
-
-    foreach ($authorElements as $authorElement) {
-        $authorName = trim($authorElement->textContent);
-        $institution = $authorElement->getAttribute('title');
-
-        $person = new Person($authorName, $institution);
-        $authors[] = $person;
-    }
-
-    $idElement = $paperCard->getElementsByTagName('div')->item(5);
-    $id = $idElement ? $idElement->textContent : '';
-
-    $typeElement = $paperCard->getElementsByTagName('div')->item(2);
-    $type = $typeElement ? $typeElement->textContent : '';
-
-    return new Paper($id, $title, $type, $authors);
-}
-
-private function writePaperDataToSheet($sheet, $paper, $row, $maxAuthors) {
-  $sheet->setCellValue('A' . $row, $paper->getId());
-  $sheet->setCellValue('B' . $row, $paper->getTitle());
-  $sheet->setCellValue('C' . $row, $paper->getType());
-
-  $authors = $paper->getAuthors();
-  $column = 'D';
-  for ($i = 0; $i < $maxAuthors; $i++) {
-      if (isset($authors[$i])) {
-          $author = $authors[$i];
-          $sheet->setCellValue($column . $row, $author->getName());
-          $column++;
-          $sheet->setCellValue($column . $row, $author->getInstitution());
-          $column++;
-      }
-  }
-}
 
 }
