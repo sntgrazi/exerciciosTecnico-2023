be28843d028a60910d6440554fe34813bc40a0a2
sntgrazi <sntgrazi22@gmail.com>
2023-11-14 22:34:26 -0300

    ajuste na logica

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -2,22 +2,68 @@
 
 namespace Chuva\Php\WebScrapping;
 
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
 /**
  * Runner for the Webscrapping exercice.
  */
-class Main {
+class Main
+{
+    /**
+     * Main runner, instantiates a Scrapper and runs.
+     */
+    public static function run(): void
+    {
+        $dom = new \DOMDocument('1.0', 'utf-8');
+        libxml_use_internal_errors(true);
 
-  /**
-   * Main runner, instantiates a Scrapper and runs.
-   */
-  public static function run(): void {
-    $dom = new \DOMDocument('1.0', 'utf-8');
-    @$dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
+        @$dom->loadHTMLFile(__DIR__.'/../../assets/origin.html');
 
-    $data = (new Scrapper())->scrap($dom);
+        $data = (new Scrapper())->scrap($dom);
 
-    // Write your logic to save the output file bellow.
-    print_r($data);
-  }
+        // Geração da planilha
 
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+
+        $sheet->setCellValue('A1', 'ID');
+        $sheet->setCellValue('B1', 'TITLE');
+        $sheet->setCellValue('C1', 'TYPE');
+
+        // Definir cabeçalhos para autores
+        $colIndex = 4; // Começar a partir da coluna D (4ª coluna)
+        $maxAuthors = max(array_map('count', array_column($data, 'authors')));
+
+        for ($i = 1; $i <= $maxAuthors; ++$i) {
+            $sheet->setCellValueByColumnAndRow($colIndex, 1, "AUTHOR $i");
+            $sheet->setCellValueByColumnAndRow($colIndex + 1, 1, "AUTHOR INSTITUTION $i");
+            $colIndex += 2; // Avançar duas colunas para o próximo autor
+        }
+
+        // Preencher dados
+        $rowIndex = 2;
+        foreach ($data as $result) {
+            $sheet->setCellValueByColumnAndRow(1, $rowIndex, $result->id);
+            $sheet->setCellValueByColumnAndRow(2, $rowIndex, $result->title);
+            $sheet->setCellValueByColumnAndRow(3, $rowIndex, $result->type);
+
+            // Preencher dados para autores
+            $colIndex = 4;
+            foreach ($result->authors as $author) {
+                $sheet->setCellValueByColumnAndRow($colIndex, $rowIndex, $author->name);
+                $sheet->setCellValueByColumnAndRow($colIndex + 1, $rowIndex, $author->institution);
+                $colIndex += 2;
+            }
+
+            ++$rowIndex;
+        }
+
+        // Salvar planilha
+        $writer = new Xlsx($spreadsheet);
+        $writer->save('paper.xlsx');
+
+        // Imprimir os dados na tela
+        print_r($data);
+    }
 }
--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -4,130 +4,48 @@ namespace Chuva\Php\WebScrapping;
 
 use Chuva\Php\WebScrapping\Entity\Paper;
 use Chuva\Php\WebScrapping\Entity\Person;
-use PhpOffice\PhpSpreadsheet\Spreadsheet;
-use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 /**
  * Does the scrapping of a webpage.
  */
-class Scrapper {
+class Scrapper
+{
+    /**
+     * Loads paper information from the HTML and returns the array with the data.
+     */
+    public function scrap(\DOMDocument $dom): array
+    {
+        $xpath = new \DOMXPath($dom);
 
-  /**
-   * Loads paper information from the HTML and returns the array with the data.
-   */
-  public function scrap(\DOMDocument $dom): array {
-    $papers = [];
+        $links = $xpath->query('//a[contains(@class, "paper-card")]');
 
-    $divElements = $dom->getElementsByTagName('div');
-    foreach ($divElements as $divElement) {
-      $classAttribute = $divElement->getAttribute('class');
-      if (strpos($classAttribute, 'col-sm-12 col-md-8 col-lg-8 col-md-pull-4 col-lg-pull-4') !== FALSE) {
-        $paperCards = $divElement->getElementsByTagName('a');
-        foreach ($paperCards as $paperCard) {
-          $paper = $this->extractPaperData($paperCard);
-          if ($paper) {
-            $papers[] = $paper;
-          }
+        $papers = [];
+
+        foreach ($links as $link) {
+            $id = explode('/', $link->getAttribute('href'))[8];
+
+            $titleElement = $link->getElementsByTagName('h4')->item(0);
+            $title = $titleElement ? $titleElement->nodeValue : '';
+
+            $typeElement = $link->getElementsByTagName('div')->item(1);
+            $typeWithId = $typeElement ? $typeElement->nodeValue : '';
+
+            preg_match('/^(.+?)(\d+)$/', $typeWithId, $matches);
+            $type = isset($matches[1]) ? trim($matches[1]) : '';
+            $id = isset($matches[2]) ? trim($matches[2]) : $id;
+
+            $authorElements = $link->getElementsByTagName('span');
+            $authors = [];
+
+            foreach ($authorElements as $authorElement) {
+                $author = $authorElement->nodeValue;
+                $institution = $authorElement->getAttribute('title');
+                $authors[] = new Person($author, $institution);
+            }
+
+            $papers[] = new Paper($id, $title, $type, $authors);
         }
-      }
+
+        return $papers;
     }
-
-    $spreadsheet = new Spreadsheet();
-    $sheet = $spreadsheet->getActiveSheet();
-
-    $sheet->setCellValue('A1', 'ID');
-    $sheet->setCellValue('B1', 'Title');
-    $sheet->setCellValue('C1', 'Type');
-
-    $row = 2;
-    $maxAuthors = 0;
-
-    foreach ($papers as $paper) {
-      $authors = $paper->getAuthors();
-      $maxAuthors = max($maxAuthors, count($authors));
-    }
-
-    $column = 'D';
-    for ($i = 1; $i <= $maxAuthors; $i++) {
-      $sheet->setCellValue($column . '1', 'Author ' . $i);
-      $column++;
-      $sheet->setCellValue($column . '1', 'Author ' . $i . ' Institution');
-      $column++;
-    }
-
-    foreach ($papers as $paper) {
-      $this->writePaperDataToSheet($sheet, $paper, $row, $maxAuthors);
-      $row++;
-    }
-
-    $excelFilePath = 'papers.xlsx';
-    $writer = new Xlsx($spreadsheet);
-    $writer->save($excelFilePath);
-
-    return $papers;
-  }
-
-  /**
-   * Este método que os dados título, autores, ID e tipo.
-   *
-   * @return \Chuva\Php\WebScrapping\Entity\Paper
-   *   O objeto Paper contendo os dados extraídos.
-   */
-  private function extractPaperData($paperCard) {
-    $titleElement = $paperCard->getElementsByTagName('h4')->item(0);
-    $title = $titleElement ? $titleElement->textContent : '';
-
-    $authorElements = $paperCard->getElementsByTagName('div')->item(0)->getElementsByTagName('span');
-    $authors = [];
-
-    foreach ($authorElements as $authorElement) {
-      $authorName = trim($authorElement->textContent);
-      $institution = $authorElement->getAttribute('title');
-
-      $person = new Person($authorName, $institution);
-      $authors[] = $person;
-    }
-
-    $idElement = $paperCard->getElementsByTagName('div')->item(5);
-    $id = $idElement ? $idElement->textContent : '';
-
-    $typeElement = $paperCard->getElementsByTagName('div')->item(2);
-    $type = $typeElement ? $typeElement->textContent : '';
-
-    return new Paper($id, $title, $type, $authors);
-  }
-
-  /**
-   * Escreve os dados do papel em uma planilha.
-   *
-   * Este método escreve os dados de um objeto Paper em uma planilha,
-   * incluindo ID, título, tipo e informações dos autores.
-   *
-   * @param \PhpOffice\PhpSpreadsheet\Worksheet\Worksheet $sheet
-   *   A planilha na qual os dados serão escritos.
-   * @param \Chuva\Php\WebScrapping\Entity\Paper $paper
-   *   O objeto Paper contendo os dados a serem escritos.
-   * @param int $row
-   *   O número da linha na planilha onde os dados serão escritos.
-   * @param int $maxAuthors
-   *   O número máximo de autores a serem incluídos na planilha.
-   */
-  private function writePaperDataToSheet($sheet, $paper, $row, $maxAuthors) {
-    $sheet->setCellValue('A' . $row, $paper->getId());
-    $sheet->setCellValue('B' . $row, $paper->getTitle());
-    $sheet->setCellValue('C' . $row, $paper->getType());
-
-    $authors = $paper->getAuthors();
-    $column = 'D';
-    for ($i = 0; $i < $maxAuthors; $i++) {
-      if (isset($authors[$i])) {
-        $author = $authors[$i];
-        $sheet->setCellValue($column . $row, $author->getName());
-        $column++;
-        $sheet->setCellValue($column . $row, $author->getInstitution());
-        $column++;
-      }
-    }
-  }
-
 }
